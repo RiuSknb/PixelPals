adae238d2c2df9147624cc1d8b1c5ed4a1eb62b9
EC2 Default User <ec2-user@ip-10-0-4-76.eu-north-1.compute.internal>
2024-12-26 05:59:23 +0000

    いいねボタンの追加

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,7 +1,31 @@
 class Public::LikesController < ApplicationController
   def create
+    @diary = Diary.find(params[:diary_id])
+    @like = current_user.likes.new(likeable: @diary)
+
+    if @like.save
+      respond_to do |format|
+        format.js # create.js.erb を探して実行
+      end
+    else
+      respond_to do |format|
+        format.js { render 'error', status: :unprocessable_entity } # エラーハンドリング用
+      end
+    end
   end
 
   def destroy
+    @diary = Diary.find(params[:diary_id])
+    @like = current_user.likes.find_by(likeable: @diary)
+
+    if @like&.destroy
+      respond_to do |format|
+        format.js # destroy.js.erb を探して実行
+      end
+    else
+      respond_to do |format|
+        format.js { render 'error', status: :unprocessable_entity } # エラーハンドリング用
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,10 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
 
+  def liked?(likeable)
+    likes.exists?(likeable: likeable)
+  end
+
 
   private
 
